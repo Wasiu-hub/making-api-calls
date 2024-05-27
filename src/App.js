fab585f965a5c7bf591e8e73cb28cf52cd1a42ab
Wasiu-hub <contactbow@yahoo.com>
2024-05-27 06:11:50 -0500

    using axios to fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import axios from "axios";
+import React, { useState } from "react";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const fetchDate = async () => {
+    let myData = await axios.get("https://jsonplaceholder.typicode.com/users");
+    console.log(myData.data);
+  };
+
+  return <button onClick={fetchDate}>fetch data</button>;
+};
 
 export default App;
+
+// axios installed (npm install axios; npm uninstall axios). When packages are installed it goes to the node_modles folder but the name will be in package.json
+// json endpoint was obtained from https://jsonplaceholder.typicode.com/
